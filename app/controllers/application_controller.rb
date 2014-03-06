b30ada3a160e871a1dde08e6b034ca9a9e08ed5a
Brad Hubbard <brad@bradhubbard.net>
2014-03-06 11:36:38 -0800

    Squelch propogating properties

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,8 @@ class ApplicationController < ActionController::Base
 	protect_from_forgery with: :exception
 
 	def flatten_with_path(obj, parent_prefix = nil)
+		# If interaction contains this, stop the propogation so we don't get junk stuff (like arrays of keywords)
+		killpoints = ["interaction.hashtags", "interaction.mention_ids", "interaction.mentions", "links.meta.content_type", "links.meta.keywords", "links.meta.opengraph", "links.meta.twitter", "links.retweet_count", "links.url", "links.content.entities", "salience.content.topics", "twitter.hashtags", "twitter.display_urls", "twitter.links", "twitter.mention_ids", "twitter.retweet.hashtags", "twitter.retweet.mention_ids", "twitter.retweet.mentions"]
 		res = {}
 
 		obj.each_with_index do |elem, i|
@@ -15,10 +17,12 @@ class ApplicationController < ActionController::Base
 
 		  key = parent_prefix ? "#{parent_prefix}.#{k}" : k # assign key name for result hash
 
-		  if v.is_a? Enumerable
-		    res.merge!(flatten_with_path(v, key)) # recursive call to flatten child elements
-		  else
-		    res[key] = v
+		  if !killpoints.include?key
+		  	  if v.is_a? Enumerable
+			    res.merge!(flatten_with_path(v, key)) # recursive call to flatten child elements
+			  else
+			    res[key] = v
+			  end
 		  end
 		end
 
