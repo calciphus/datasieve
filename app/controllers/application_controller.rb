96c00530f67ec3c6d61bd97727564d7d91f3b796
Brad Hubbard <brad@bradhubbard.net>
2014-03-06 13:51:32 -0800

    Readd killpoints

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,14 @@ class ApplicationController < ActionController::Base
 	# For APIs, you may want to use :null_session instead.
 	protect_from_forgery with: :exception
 
+	def is_num?(str)
+	  begin
+	    !!Integer(str)
+	  rescue ArgumentError, TypeError
+	    false
+	  end
+	end
+
 	def flatten_with_path(obj, parent_prefix = nil)
 		# If interaction contains this, stop the propogation so we don't get junk stuff (like arrays of keywords)
 		killpoints = ["interaction.hashtags", "interaction.mention_ids", "interaction.mentions", "links.meta.content_type", "links.meta.keywords", "links.meta.opengraph", "links.meta.twitter", "links.retweet_count", "links.url", "links.content.entities", "salience.content.topics", "twitter.hashtags", "twitter.display_urls", "twitter.links", "twitter.mentions","twitter.mention_ids", "twitter.retweet.hashtags", "twitter.retweet.mention_ids", "twitter.retweet.mentions","salience.content.entities.themes"]
@@ -17,13 +25,19 @@ class ApplicationController < ActionController::Base
 
 		  key = parent_prefix ? "#{parent_prefix}.#{k}" : k # assign key name for result hash
 
-		  if !killpoints.include?key
+		  if !is_num?k and !killpoints.include?key
 		  	  if v.is_a? Enumerable
 			    res.merge!(flatten_with_path(v, key)) # recursive call to flatten child elements
 			  else
 			    res[key] = v
 			  end
+		  else
+		  	# This is an iterated array, will be caught when only one result is returned
+			# Or this is one of our killpoints
+			# puts "#{k} :: #{v}"
+			# puts "\n"
 		  end
+
 		end
 
 		res
